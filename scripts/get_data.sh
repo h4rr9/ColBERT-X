b1612e3eafff76c7f9ab86ee4252b49b871fd799
Hariganesh Srinivasan <hariganeshs1999@gmail.com>
2023-04-21 13:52:17 -0400

    added pretraining data download

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -7,3 +7,7 @@ wget -c "https://huggingface.co/datasets/unicamp-dl/mmarco/resolve/main/data/goo
 wget -c "https://huggingface.co/datasets/unicamp-dl/mmarco/resolve/main/data/google/queries/train/english_queries.train.tsv" # english queries
 
 wget -c "https://huggingface.co/datasets/unicamp-dl/mmarco/resolve/main/data/google/queries/train/chinese_queries.train.tsv" # chinese queries
+
+
+wget -c "https://huggingface.co/datasets/h4rr9/mmarco_pretrain_mini/resolve/main/triples.pretrain.doc.mini.tsv"
+wget -c "https://huggingface.co/datasets/h4rr9/mmarco_pretrain_mini/resolve/main/triples.pretrain.query.mini.tsv"
