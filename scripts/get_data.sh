a1f84d8a05e3b29a03dcd31493939479ffb4f8ea
Hariganesh Srinivasan <hariganeshs1999@gmail.com>
2023-04-21 09:33:51 -0400

    added get_data.sh

---
+++ scripts/get_data.sh
@@ -0,0 +1,9 @@
+wget -c "https://huggingface.co/datasets/unicamp-dl/mmarco/resolve/main/data/triples.train.ids.small.tsv" # triples
+
+wget -c "https://huggingface.co/datasets/unicamp-dl/mmarco/resolve/main/data/google/collections/english_collection.tsv" # english collection
+
+wget -c "https://huggingface.co/datasets/unicamp-dl/mmarco/resolve/main/data/google/collections/chinese_collection.tsv" # chinese collection
+
+wget -c "https://huggingface.co/datasets/unicamp-dl/mmarco/resolve/main/data/google/queries/train/english_queries.train.tsv" # english queries
+
+wget -c "https://huggingface.co/datasets/unicamp-dl/mmarco/resolve/main/data/google/queries/train/chinese_queries.train.tsv" # chinese queries
